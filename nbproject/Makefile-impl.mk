4847ad21ec57634eb6ea9039db57efb98bc4012a
Joshua Wolbeck <wallywolbeck@gmail.com>
2018-11-27 01:33:55 -0600

    Desktop commit

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=mplabs_lux_sensor
+PROJECTNAME=SmartIr_Hardware
 
 # Active Configuration
 DEFAULTCONF=default
